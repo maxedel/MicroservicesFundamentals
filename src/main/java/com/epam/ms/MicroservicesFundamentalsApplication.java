56a0d8ff721e2b1f755301c12c5917aa8649b758
Maxim Peters <max_edel@mail.ru>
2022-09-29 16:34:06 +0200

    Init commit

---
+++ src/main/java/com/epam/ms/MicroservicesFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/ms/MicroservicesFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
